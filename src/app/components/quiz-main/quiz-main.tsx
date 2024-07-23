11c1f0b6761f3edaa748a8f942aafc8ee102bcd4
iwanttobealight <iwanttobealight@yandex.ru>
2024-07-23 14:12:12 +0200

    refactoring

--- src/app/components/quiz-main/quiz-main.tsx
+++ src/app/components/quiz-main/quiz-main.tsx
@@ -5,7 +5,6 @@ import sample from "lodash.sample";
 import sampleSize from "lodash.samplesize";
 import styles from "./quiz-main.module.css";
 
-import { QuizButtons } from "../quiz-dynamic/quiz-buttons";
 import { Button } from "../ui/button/button";
 
 const MAX_ROUND_NUM = 2;
@@ -25,6 +24,21 @@ export const QuizMain: React.FC<QuizMainProps> = ({ countries }) => {
   const [correctOpt, setCorrectOpt] = useState<Country>();
   const [count, setCount] = useState(0);
   const [round, setRound] = useState(0);
+  const [chosenOpt, setChosenOpt] = useState<Country | null>(null);
+
+  const onButtonClick = (country: Country) => {
+    setChosenOpt(country);
+
+    if (country?.name === correctOpt?.name) {
+      setCount((prev) => prev + 1);
+    }
+    setRound((prev) => prev + 1);
+  };
+
+  const resetState = () => {
+    setChosenOpt(null);
+    setInitialState();
+  };
 
   const setInitialState = useCallback(() => {
     if (!countries) return;
@@ -41,16 +55,24 @@ export const QuizMain: React.FC<QuizMainProps> = ({ countries }) => {
   // todo: add stub
   if (!options) return "loading.....";
 
+  // todo: work on the last screen
   if (round >= MAX_ROUND_NUM) {
     return (
       <>
         <h1>G A M E O V E R</h1>
         <p>
-          your result: ${count} correct answers of {MAX_ROUND_NUM}
+          your result: <h3>{count}</h3> correct answers of {MAX_ROUND_NUM}
         </p>
         <br />
         <br />
-        <Button onClick={setInitialState}>start again</Button>
+        <Button
+          onClick={() => {
+            resetState();
+            setRound(0);
+          }}
+        >
+          start again
+        </Button>
       </>
     );
   }
@@ -70,13 +92,41 @@ export const QuizMain: React.FC<QuizMainProps> = ({ countries }) => {
         // todo: move to func
         src={`https://flagcdn.com/${correctOpt?.code?.toLowerCase()}.svg`}
       />
-      <QuizButtons
-        onNext={setInitialState}
-        correctOpt={correctOpt}
-        options={options}
-        updateCount={setCount}
-        setRound={setRound}
-      />
+      <div>
+        <div className={styles.buttons}>
+          {options?.map((opt, i) => (
+            <Button
+              key={i}
+              disabled={!!chosenOpt}
+              onClick={() => onButtonClick(opt)}
+              status={
+                chosenOpt ? getStatus(opt, chosenOpt, correctOpt) : "unset"
+              }
+            >
+              {opt.name}
+            </Button>
+          ))}
+        </div>
+        <br />
+        <br />
+        {chosenOpt && <Button onClick={resetState}>next</Button>}
+      </div>
     </div>
   );
+};
+
+const getStatus = (opt: Country, chosenOpt?: Country, correctOpt?: Country) => {
+  const isGuessCorrect = chosenOpt?.name === correctOpt?.name;
+  const isCurOptChosen = opt.name === chosenOpt?.name;
+  const isCurOptCorrect = opt.name === correctOpt?.name;
+
+  if (isCurOptCorrect) {
+    return "correct";
+  }
+
+  if (isCurOptChosen) {
+    return isGuessCorrect ? "correct" : "wrong";
+  }
+
+  return "unset";
 };
